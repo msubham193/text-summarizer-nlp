83bdb0ec68c009b0ed70baaf9f1d503fff504caf
Subham Mishra <63531369+msubham193@users.noreply.github.com>
2023-03-16 05:28:21 +0530

    ccc

--- app.py
+++ app.py
@@ -15,33 +15,35 @@ def summarizer(text):
     tokens = [token.text for token in doc]
     word_freq = {}
 
-    for word in doc:
-        if word.text.lower() not in stop_words and word.text.lower() not in punctuation:
-            if word.text not in word_freq.keys():
-                word_freq[word.text] = 1
-            else:
-                word_freq[word.text] += 1
+    return word_freq
 
-    max_freq = max(word_freq.values())
-    for word in word_freq:
-        word_freq[word] = word_freq[word]/max_freq
+    # for word in doc:
+    #     if word.text.lower() not in stop_words and word.text.lower() not in punctuation:
+    #         if word.text not in word_freq.keys():
+    #             word_freq[word.text] = 1
+    #         else:
+    #             word_freq[word.text] += 1
 
-    sent_token = [sent for sent in doc.sents]
+    # max_freq = max(word_freq.values())
+    # for word in word_freq:
+    #     word_freq[word] = word_freq[word]/max_freq
 
-    sent_scores = {}
+    # sent_token = [sent for sent in doc.sents]
 
-    for sent in sent_token:
-        for word in sent:
-            if word.text in word_freq.keys():
-                if sent not in sent_scores.keys():
-                    sent_scores[sent] = word_freq[word.text]
-                else:
-                    sent_scores[sent] += word_freq[word.text]
+    # sent_scores = {}
 
-    select_len = int(len(sent_token)*0.3)
-    summary = nlargest(select_len, sent_scores, key=sent_scores.get)
-    final_summary = [word.text for word in summary]
-    summary = ' '.join(final_summary)
+    # for sent in sent_token:
+    #     for word in sent:
+    #         if word.text in word_freq.keys():
+    #             if sent not in sent_scores.keys():
+    #                 sent_scores[sent] = word_freq[word.text]
+    #             else:
+    #                 sent_scores[sent] += word_freq[word.text]
+
+    # select_len = int(len(sent_token)*0.3)
+    # summary = nlargest(select_len, sent_scores, key=sent_scores.get)
+    # final_summary = [word.text for word in summary]
+    # summary = ' '.join(final_summary)
 
     return summary
 
